6ee62ec9c406384e0146fc4dc1c3532a885363b7
MitchDeRoo <deroom01@gmail.com>
2024-10-20 18:45:11 +1100

    feat: Adds BingoDocumentWriter

---
+++ MusicBingo/MusicBingo.Tests/BingoDocumentWriterShould.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using MusicBingo.BingoCards;
+using MusicBingo.Writers;
+
+namespace MusicBingo.Tests;
+
+[TestFixture]
+public class BingoDocumentWriterShould
+{
+    [Test]
+    public void Return_MemoryStream()
+    {
+        var writer = new BingoDocumentWriter();
+        var cards = new List<BingoCard>
+        {
+            new()
+            {
+                Title = "Example Song",
+                Cells =
+                [
+                    ["B", "I", "N", "G", "O"],
+                    ["Song A", "Song B", "Song C", "", "Song D"],
+                    ["Song E", "Song F", "Song G", "Song H", "Song I"],
+                    ["Song J", "Song K", "Free Space", "Song L", "Song M"],
+                    ["Song N", "Song O", "Song P", "Song Q", "Song R"],
+                    ["Song S", "Song T", "Song U", "Song V", "Song W"]
+                ]
+            }
+        };
+
+        var memoryStream = writer.WriteBingoCardsToDocument(cards);
+
+        memoryStream.Should().BeOfType<MemoryStream>();
+        memoryStream.Length.Should().BeGreaterThan(0);
+    }
+}
---
+++ MusicBingo/MusicBingo/Writers/BingoDocumentWriter.cs
@@ -0,0 +1,34 @@
+using MusicBingo.BingoCards;
+using Xceed.Words.NET;
+
+namespace MusicBingo.Writers;
+
+public class BingoDocumentWriter
+{
+    public MemoryStream WriteBingoCardsToDocument(List<BingoCard> cards)
+    {
+        var memoryStream = new MemoryStream();
+
+        using (var document = DocX.Create(memoryStream))
+        {
+            foreach (var bingoCard in cards)
+            {
+                document.InsertParagraph(bingoCard.Title).FontSize(16).Bold().Spacing(10);
+                var table = document.InsertTable(6, 5);
+
+                for (int row = 0; row < 6; row++)
+                {
+                    for (int col = 0; col < 5; col++)
+                    {
+                        table.Rows[row].Cells[col].Paragraphs[0].Append(bingoCard.Cells[row][col]);
+                    }
+                }
+                document.InsertParagraph().SpacingAfter(10);
+            }
+            document.Save();
+        }
+
+        memoryStream.Position = 0;
+        return memoryStream;
+    }
+}
